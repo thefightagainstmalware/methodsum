a9ddc6e6a77c0a7ecb54903a2a86d3c50e95627f
PandaNinjas <admin@malwarefight.gq>
2022-09-28 21:08:41 -0700

    Add compiled binaries

---
+++ js/pkg/methodsum4rust.d.ts
@@ -0,0 +1,49 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class MethodSum {
+  free(): void;
+/**
+* @param {Uint8Array} insns
+* @returns {MethodSum}
+*/
+  static init(insns: Uint8Array): MethodSum;
+/**
+* @returns {string}
+*/
+  get_hash(): string;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_methodsum_free: (a: number) => void;
+  readonly methodsum_init: (a: number, b: number) => number;
+  readonly methodsum_get_hash: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ js/pkg/methodsum4rust_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_methodsum_free(a: number): void;
+export function methodsum_init(a: number, b: number): number;
+export function methodsum_get_hash(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
